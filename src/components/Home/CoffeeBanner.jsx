20ea19b2bd1532ab097aa3b44da7dcfadaf9b747
Keti1501 <katjarost1501@gmail.com>
2025-08-17 18:59:49 +0300

    changes in the public folder

--- src/components/Home/CoffeeBanner.jsx
+++ src/components/Home/CoffeeBanner.jsx
@@ -1,33 +1,29 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { Box, Typography, Button } from '@mui/material';
 import imageBg from '../../assets/images/home/imagebg.png';
-import Bannervideo from '../../assets/videos/bannervideopreview.mp4';
-import preview from '../../assets/videos/preview.jpg';
 import { btnStyles } from '../../styles/btnStyles.jsx';
 import { headTitle } from '../../styles/typographyStyles.jsx';
-import tornbottombg from '../../assets/videos/tornbottombg.svg';
+import tornbottombg from '../../assets/images/home/tornbottombg.svg';
 
 
 const CoffeeBanner = () => {
-    const [videoLoaded, setVideoLoaded] = useState(false);
+    
+    const videoRef = useRef(null);
 
     useEffect(() => {
-        const video = document.createElement('video');
-        video.src = Bannervideo;
-        video.loop = true;
-        video.muted = true;
-        video.autoplay = true;
-        video.playsInline = true;
-
-        video.oncanplaythrough = () => setVideoLoaded(true);
+        if (videoRef.current) {
+            videoRef.current.load();
+            const playPromise = videoRef.current.play();
+            if (playPromise !== undefined) {
+                playPromise.catch(() => {});
+            }
+        }
     }, []);
 
     return (
-        <Box sx={{ position: 'relative', width: '100%', height: 600, overflow: 'hidden', }}>
+        <Box sx={{ position: 'relative', width: '100%', height: 600, overflow: 'hidden', backgroundImage: 'url(/images/preview.jpg)', backgroundSize: 'cover', backgroundPosition: 'center', }}>
 
-            <Box component="img" src={preview} alt="preview" sx={{ width: '100%', height: '100%', objectFit: 'cover', zIndex: 0, inset: 0, }} loading="eager" />
-
-            <video src={Bannervideo} autoPlay loop muted playsInline style={{ width: '100%', height: '100%', objectFit: 'cover', inset: 0, zIndex: 1, opacity: videoLoaded ? 1 : 0, transition: "opacity 0.5s ease-in-out", }} onCanPlayThrough={() => setVideoLoaded(true)} />
+            <video ref={videoRef} src="/videos/bannervideopreview.mp4" poster="/images/preview.jpg" autoPlay loop muted playsInline preload="auto" style={{ position: "absolute", top: 0, left: 0, width: '100%', height: '100%', objectFit: 'cover', zIndex: 1,}} />
 
             <Box component="img" src={tornbottombg} alt="tornbottombg" sx={{ position: 'absolute', bottom: 0, left: 0, width: '100%', height: 'auto', zIndex: 3, pointerEvents: 'none', }} />
             <Box sx={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', zIndex: 2, display: 'flex', }} >
